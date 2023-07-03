de803a69151f64b37c562de89c0300bcd1d80c2b
Noman Ali <noman.sd114@gmail.com>
2023-07-03 07:52:22 +0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/_shared/header/header.component';
+import { BannerComponent } from './components/index/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule
