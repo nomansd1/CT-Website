59bc06e949f44b61d278905c78ad3cf6b6bf5e5d
Noman Ali <noman.sd114@gmail.com>
2023-07-07 14:21:55 +0500

    about page completed

--- src/app/components/about/our-values/our-values.component.ts
+++ src/app/components/about/our-values/our-values.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./our-values.component.css']
 })
 export class OurValuesComponent {
-
+  valuesGridTiles = [
+    {
+      icon: '../../../../assets/abt1.png',
+      title: 'Integrity',
+      desc: 'Do the right thing, Take responsibility, Do what you say.',
+    },
+    {
+      icon: '../../../../assets/abt2.png',
+      title: 'Excellence',
+      desc: 'Plan ahead, Contiously inmporve, Sweat the details.',
+    },
+    {
+      icon: '../../../../assets/abt3.png',
+      title: 'Teamwork',
+      desc: 'Trust eachother, Work together, Be coachable.',
+    },
+    {
+      icon: '../../../../assets/abt4.png',
+      title: 'Passion',
+      desc: 'Serve the coach, Support the partner, Deliver success.',
+    },
+    {
+      icon: '../../../../assets/abt5.png',
+      title: 'Win',
+      desc: 'Fight hard, Never quit, Finish',
+    },
+  ]
 }
