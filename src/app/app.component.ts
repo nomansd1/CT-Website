21ab55742bc7980fadf3a387d2224304eba17e47
nomansd1 <noman.sd114@gmail.com>
2023-07-26 16:43:42 +0500

    working on validations on demo page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements AfterViewInit, OnInit {
   ngOnInit() {
     setTimeout(() => {
       this.isLoading = false;
-    }, 3000); 
+    }, 1000); 
   }
   ngAfterViewInit(): void {
     register();
--- src/app/components/demo/demo-form/demo-form.component.ts
+++ src/app/components/demo/demo-form/demo-form.component.ts
@@ -13,14 +13,14 @@ export class DemoFormComponent {
   
   productName: string | null = null;
   
-  name!: string;
-  businessEmail!: string;
-  country!: string;
-  contact!: string;
-  company!: string;
-  industry!: string;
+  name: string = '';
+  businessEmail: string = '';
+  country: string = '';
+  contact: string = '';
+  company: string = '';
+  industry: string = '';
   solutions: any;
-  message!: string;
+  message: string = '';
 
   demoOptions = [
     'S&D Next',
@@ -36,10 +36,22 @@ export class DemoFormComponent {
     'Primary+',
   ]
 
+  validateMessage = { 
+    msg: 'Please fill out the missing fields',
+    state: false
+  };
+  successMessage = {
+    msg: `Thank You for Contacting Us!\n We have received your message and will get back to you shortly.`,
+    state: false
+  }
+
   constructor(
     private route: ActivatedRoute,
     private emailService: EmailService
-    ) {}
+    ) {
+      console.log(this,this.successMessage);
+      
+    }
 
   ngOnInit(): void {
     // Retrieve the product name from the route parameters
@@ -47,27 +59,21 @@ export class DemoFormComponent {
       this.productName = params['productName'] || null;
     });
     this.solutions = this.productName;
-    console.log(this.productName); 
-  }
-
-  submit() {
-    console.log('name', this.name);
-    console.log('b email', this.businessEmail);
-    console.log('country', this.country);
-    console.log('company', this.company);
-    console.log('industry', this.industry);
-    console.log('solutions', this.solutions);
-    console.log('message', this.message);
-    
   }
 
   onSubmit() {
+    if (this.isFormValid() == false) {
+      this.validateMessage.state = true;
+      console.log("validation condition is running");
+      
+      return;
+    }
     // Get the form values
     const formData = {
       CompanyCode: 61,
       OfficeCode: 100061,
       Subject: 'Software Demo Request',
-      CC: 'erum.azam@cloudtenants.com',
+      CC: 'noman.sd114@gmail.com',
       FromNames: 'noreply@cloudtenants.com',
       ToNames: 'noman.ali@cloudtenants.com',
       Body: '',
@@ -94,5 +100,39 @@ export class DemoFormComponent {
         // You can handle the error here (e.g., show an error message)
       }
     );
+    this.successMessage.state = true;
+  }
+
+  isFormValid(): boolean {
+    const emailExpression: RegExp = /^(?=.{1,254}$)(?=.{1,64}@)[-!#$%&'*+/0-9=?A-Z^_`a-z{|}~]+(\.[-!#$%&'*+/0-9=?A-Z^_`a-z{|}~]+)*@[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?(\.[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?)*$/;
+    let validEmail = emailExpression.test(this.businessEmail);
+    let validName = this.name.length == 0;
+    let validEmail2 = this.businessEmail.length == 0;
+    let validCompany = this.company.length == 0;
+    let validContact = this.contact.length == 0;
+    let validCountry = this.country.length == 0;
+    let validIndustry = this.industry.length == 0;
+    let validMessage = this.message.length == 0;
+
+    console.log("email: ", this,this.businessEmail);
+    console.log("name: ", this.name);
+    console.log("company: ", this.company);
+    console.log("country: ", this.country);
+    console.log("industry: ", this.industry);
+    console.log("message: ", this.message);
+    console.log("contact: ", this.contact);
+    
+    console.log("before submit");
+    console.log("email: ",validEmail);
+    console.log("email empty: ",validEmail2);
+    console.log("name: ",validName);
+    console.log("company: ",validCompany);
+    console.log("country: ",validCountry);
+    console.log("industry: ",validIndustry);
+    console.log("message: ",validMessage);
+    console.log("contact: ",validContact);
+
+    
+    return (validEmail && validName && validEmail2 && validCompany && validContact && validCountry && validIndustry && validMessage)
   }
 }
