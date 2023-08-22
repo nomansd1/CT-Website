86a9f945f4003c2e2d532e9f6227556baa08deae
nomansd1 <noman.sd114@gmail.com>
2023-08-22 11:08:16 +0500

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { IndexLayoutComponent } from './components/index/index-layout/index-layout.component';
 import { AboutLayoutComponent } from './components/about/about-layout/about-layout.component';
 import { CareersLayoutComponent } from './components/careers/careers-layout/careers-layout.component';
