8bfaea70cb63d48d856bdcc3d3b3191dc1ec00af
Noman Ali <noman.sd114@gmail.com>
2023-11-30 17:22:07 +0500

    import feature page updated

--- src/app/components/import/imp-grid/imp-grid.component.ts
+++ src/app/components/import/imp-grid/imp-grid.component.ts
@@ -6,17 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./imp-grid.component.css']
 })
 export class ImpGridComponent {
-  tiles =[
-    { url: '../../../../assets/imp1.png', title: 'import purchase order' },
-    { url: '../../../../assets/imp2.png', title: 'import duty & taxes' },
-    { url: '../../../../assets/imp3.png', title: 'LC opening' },
-    { url: '../../../../assets/imp4.png', title: 'LC arrival' },
-    { url: '../../../../assets/imp5.png', title: 'import GRN' },
-    { url: '../../../../assets/imp6.png', title: 'duties & taxes management' },
-    { url: '../../../../assets/imp7.png', title: 'landed cost' },
-    { url: '../../../../assets/imp8.png', title: 'multi currency setup' },
-    { url: '../../../../assets/imp9.png', title: 'duty & taxes setup' },
-    { url: '../../../../assets/imp10.png', title: 'integration with finance module' },
-    { url: '../../../../assets/imp11.png', title: 'ledger maintained on foreign & local currency' },
-  ]
+  tiles = [
+    { url: '../../../../assets/imp1.png', title: 'Import Purchase Order' },
+    { url: '../../../../assets/imp2.png', title: 'Import Duty & Taxes' },
+    { url: '../../../../assets/imp3.png', title: 'LC Opening' },
+    { url: '../../../../assets/imp4.png', title: 'LC Arrival' },
+    { url: '../../../../assets/imp5.png', title: 'Import GRN' },
+    { url: '../../../../assets/imp6.png', title: 'Duties & Taxes Management' },
+    { url: '../../../../assets/imp7.png', title: 'Landed Cost' },
+    { url: '../../../../assets/imp8.png', title: 'Multi-Currency Setup' },
+    { url: '../../../../assets/imp9.png', title: 'Duty & Taxes Setup' },
+    { url: '../../../../assets/imp10.png', title: 'Integration with Finance Module' },
+    { url: '../../../../assets/imp11.png', title: 'Ledger Maintained on Foreign & Local Currency' },
+  ];  
 }
