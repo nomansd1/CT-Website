c94dc9ab425b4708d3bff223945c41c9e6df9328
Noman Ali <noman.sd114@gmail.com>
2023-11-30 18:09:56 +0500

    updated scm

--- src/app/components/scn+/scm-grid/scm-grid.component.ts
+++ src/app/components/scn+/scm-grid/scm-grid.component.ts
@@ -6,40 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./scm-grid.component.css']
 })
 export class ScmGridComponent {
-  tiles =[
-    { url: '../../../../assets/scm1.png', title: 'product management' },
-    { url: '../../../../assets/scm2.png', title: 'suppliers management' },
-    { url: '../../../../assets/scm3.png', title: 'multi warehouse setup' },
-    { url: '../../../../assets/scm4.png', title: 'multi company setups' },
-    { url: '../../../../assets/scm5.png', title: 'multi users setups' },
-    { url: '../../../../assets/scm6.png', title: 'batch & expiry management' },
-    { url: '../../../../assets/scm7.png', title: 'barcode module' },
-    { url: '../../../../assets/scm8.png', title: 'requisition' },
-    { url: '../../../../assets/scm9.png', title: 'quotation' },
-    { url: '../../../../assets/scm10.png', title: 'comparatives' },
-    { url: '../../../../assets/scm11.png', title: 'purchase order' },
-    { url: '../../../../assets/scm12.png', title: 'inspection' },
-    { url: '../../../../assets/scm13.png', title: 'goods received' },
-    { url: '../../../../assets/scm14.png', title: 'goods return' },
-    { url: '../../../../assets/scm15.png', title: 'demand' },
-    { url: '../../../../assets/scm16.png', title: 'transfer in' },
-    { url: '../../../../assets/scm17.png', title: 'transfer out' },
-    { url: '../../../../assets/scm18.png', title: 'scanning option' },
-    { url: '../../../../assets/scm19.png', title: 'pallet, location, batch wise stock position' },
-    { url: '../../../../assets/scm11.png', title: 'accurate stock control' },
-    { url: '../../../../assets/scm21.png', title: 'easy product traceability' },
-    { url: '../../../../assets/scm22.png', title: 'HHT stock audit' },
-    { url: '../../../../assets/scm23.png', title: 'power BI for management' },
-  ]
-
+  tiles = [
+    { url: '../../../../assets/scm1.png', title: 'Product Management' },
+    { url: '../../../../assets/scm2.png', title: 'Suppliers Management' },
+    { url: '../../../../assets/scm3.png', title: 'Multi-Warehouse Setup' },
+    { url: '../../../../assets/scm4.png', title: 'Multi-Company Setup' },
+    { url: '../../../../assets/scm5.png', title: 'Multi-Users Setup' },
+    { url: '../../../../assets/scm6.png', title: 'Batch and Expiry Management' },
+    { url: '../../../../assets/scm7.png', title: 'Barcode Module' },
+    { url: '../../../../assets/scm8.png', title: 'Requisition' },
+    { url: '../../../../assets/scm9.png', title: 'Quotation' },
+    { url: '../../../../assets/scm10.png', title: 'Comparatives' },
+    { url: '../../../../assets/scm11.png', title: 'Purchase Order' },
+    { url: '../../../../assets/scm12.png', title: 'Inspection' },
+    { url: '../../../../assets/scm13.png', title: 'Goods Received' },
+    { url: '../../../../assets/scm14.png', title: 'Goods Return' },
+    { url: '../../../../assets/scm15.png', title: 'Demand' },
+    { url: '../../../../assets/scm16.png', title: 'Transfer In' },
+    { url: '../../../../assets/scm17.png', title: 'Transfer Out' },
+    { url: '../../../../assets/scm18.png', title: 'Scanning Option' },
+    { url: '../../../../assets/scm19.png', title: 'Pallet, Location, Batch-wise Stock Position' },
+    { url: '../../../../assets/scm20.png', title: 'Accurate Stock Control' },
+    { url: '../../../../assets/scm21.png', title: 'Easy Product Traceability' },
+    { url: '../../../../assets/scm22.png', title: 'HHT Stock Audit' },
+    { url: '../../../../assets/scm23.png', title: 'Power BI for Management' },
+  ];
+  
   tiles2 = [
-    { url: '../../../../assets/scm19.png', title: 'purchase requisition' },
-    { url: '../../../../assets/scm11.png', title: 'purchase order' },
+    { url: '../../../../assets/scm19.png', title: 'Purchase Requisition' },
+    { url: '../../../../assets/scm20.png', title: 'Purchase Order' },
     { url: '../../../../assets/scm21.png', title: 'GRN' },
-    { url: '../../../../assets/scm18.png', title: 'goods received' },
-    { url: '../../../../assets/scm22.png', title: 'HHT stock audit' },
-    { url: '../../../../assets/scm15.png', title: 'demand' },
-    { url: '../../../../assets/scm16.png', title: 'transfer in' },
-    { url: '../../../../assets/scm17.png', title: 'transfer out' },
-  ]
+    { url: '../../../../assets/scm18.png', title: 'Goods Received' },
+    { url: '../../../../assets/scm22.png', title: 'HHT Stock Audit' },
+    { url: '../../../../assets/scm15.png', title: 'Demand' },
+    { url: '../../../../assets/scm16.png', title: 'Transfer In' },
+    { url: '../../../../assets/scm17.png', title: 'Transfer Out' },
+  ];
+  
 }
