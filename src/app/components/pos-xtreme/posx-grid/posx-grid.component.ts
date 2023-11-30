54ff9c951283fa6a9689f90f2a3ea7a2c5092941
Noman Ali <noman.sd114@gmail.com>
2023-11-30 17:33:33 +0500

    updated pos xtreme feature page

--- src/app/components/pos-xtreme/posx-grid/posx-grid.component.ts
+++ src/app/components/pos-xtreme/posx-grid/posx-grid.component.ts
@@ -6,37 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./posx-grid.component.css']
 })
 export class PosxGridComponent {
-  tiles =[
-    { url: '../../../../assets/posx1.png', title: 'multi outlets management' },
-    { url: '../../../../assets/posx2.png', title: 'multi warehouse management' },
-    { url: '../../../../assets/posx3.png', title: 'comprehensive user roles amd rights management' },
-    { url: '../../../../assets/posx4.png', title: 'comprehensive price management module' },
-    { url: '../../../../assets/posx5.png', title: 'multiple payments modes' },
-    { url: '../../../../assets/posx6.png', title: 'banks machine integration' },
-    { url: '../../../../assets/posx7.png', title: 'comprehensive discounts and schemes engine' },
-    { url: '../../../../assets/posx8.png', title: 'comprehensive sales module' },
-    { url: '../../../../assets/posx9.png', title: 'offline tills' },
-    { url: '../../../../assets/posx10.png', title: 'management workflow approvals' },
-    { url: '../../../../assets/posx11.png', title: 'customer management' },
-    { url: '../../../../assets/posx12.png', title: 'digital weighing scale integration' },
-    { url: '../../../../assets/posx13.png', title: 'comprehensive loyalty points module' },
-    { url: '../../../../assets/posx14.png', title: 'gift card/bank voucher' },
-    { url: '../../../../assets/posx15.png', title: 'price checker' },
-    { url: '../../../../assets/posx16.png', title: 'integration with government tax portal' },
-    { url: '../../../../assets/posx17.png', title: 'digital payments gateways/apps integration' },
-    { url: '../../../../assets/posx18.png', title: 'whatsapp API integration' },
-    { url: '../../../../assets/posx19.png', title: 'SMS API integration' },
-    { url: '../../../../assets/posx20.png', title: 'MS reports/management dashboards' },
-  ]
+  tiles = [
+    { url: '../../../../assets/posx1.png', title: 'Multi Outlets Management' },
+    { url: '../../../../assets/posx2.png', title: 'Multi Warehouse Management' },
+    { url: '../../../../assets/posx3.png', title: 'Comprehensive User Roles and Rights Management' },
+    { url: '../../../../assets/posx4.png', title: 'Comprehensive Price Management Module' },
+    { url: '../../../../assets/posx5.png', title: 'Multiple Payments Modes' },
+    { url: '../../../../assets/posx6.png', title: 'Banks Machine Integration' },
+    { url: '../../../../assets/posx7.png', title: 'Comprehensive Discounts and Schemes Engine' },
+    { url: '../../../../assets/posx8.png', title: 'Comprehensive Sales Module' },
+    { url: '../../../../assets/posx9.png', title: 'Offline Tills' },
+    { url: '../../../../assets/posx10.png', title: 'Management Workflow Approvals' },
+    { url: '../../../../assets/posx11.png', title: 'Customer Management' },
+    { url: '../../../../assets/posx12.png', title: 'Digital Weighing Scale Integration' },
+    { url: '../../../../assets/posx13.png', title: 'Comprehensive Loyalty Points Module' },
+    { url: '../../../../assets/posx14.png', title: 'Gift Card/Bank Voucher' },
+    { url: '../../../../assets/posx15.png', title: 'Price Checker' },
+    { url: '../../../../assets/posx16.png', title: 'Integration with Government Tax Portal' },
+    { url: '../../../../assets/posx17.png', title: 'Digital Payments Gateways/Apps Integration' },
+    { url: '../../../../assets/posx18.png', title: 'WhatsApp API Integration' },
+    { url: '../../../../assets/posx19.png', title: 'SMS API Integration' },
+    { url: '../../../../assets/posx20.png', title: 'MS Reports/Management Dashboard' },
+  ];
+  
   tiles2 = [
-    { url: '../../../../assets/posx21.png', title: 'E-Commerce portal' },
-    { url: '../../../../assets/posx22.png', title: '3rd party E-Commerce integration' },
-    { url: '../../../../assets/posx23.png', title: 'E-Shop application (customer ordering & loyalty app)' },
-    { url: '../../../../assets/posx24.png', title: 'stock management & audit app' },
-  ]
+    { url: '../../../../assets/posx21.png', title: 'E-Commerce Portal' },
+    { url: '../../../../assets/posx22.png', title: '3rd Party E-Commerce Integration' },
+    { url: '../../../../assets/posx23.png', title: 'E-Shop Application (Customer Ordering & Loyalty App)' },
+    { url: '../../../../assets/posx24.png', title: 'Stock Management & Audit App' },
+  ];
+  
   tiles3 = [
-    { url: '../../../../assets/posx25.png', title: 'comprehensive supply chain module' },
-    { url: '../../../../assets/posx26.png', title: 'comprehensive GL finance module' },
-    { url: '../../../../assets/posx27.png', title: 'comprehensive HR/payroll module' },
-  ]
+    { url: '../../../../assets/posx25.png', title: 'Comprehensive Supply Chain Module' },
+    { url: '../../../../assets/posx26.png', title: 'Comprehensive GL Finance Module' },
+    { url: '../../../../assets/posx27.png', title: 'Comprehensive HR/Payroll Module' },
+  ];
+  
 }
