43924b98d10d1a63184e0bbb38d4d730be63168f
Noman Ali <noman.sd114@gmail.com>
2023-11-30 17:52:50 +0500

    snd esuite page updated

--- src/app/components/s&d-esuite/snde-grid/snde-grid.component.ts
+++ src/app/components/s&d-esuite/snde-grid/snde-grid.component.ts
@@ -7,77 +7,84 @@ import { Component } from '@angular/core';
 })
 export class SndeGridComponent {
   tiles = [
-    { url: '../../../../assets/snde1.png', title: 'system suggested replenishment planning' },
-    { url: '../../../../assets/snde2.png', title: 'X-days order bucket' },
-    { url: '../../../../assets/snde3.png', title: 'distributor order portal' },
-    { url: '../../../../assets/snde4.png', title: 'reduced process cost' },
-    { url: '../../../../assets/snde5.png', title: 'higher services levels' },
-    { url: '../../../../assets/snde6.png', title: 'optimized stock levels' },
-    { url: '../../../../assets/snde7.png', title: 'cost effective transportation' },
-  ]
+    { url: '../../../../assets/snde1.png', title: 'System Suggested Replenishment Planning' },
+    { url: '../../../../assets/snde2.png', title: 'X-Days Order Bucket' },
+    { url: '../../../../assets/snde3.png', title: 'Distributor Order Portal' },
+    { url: '../../../../assets/snde4.png', title: 'Reduced Process Cost' },
+    { url: '../../../../assets/snde5.png', title: 'Higher Service Levels' },
+    { url: '../../../../assets/snde6.png', title: 'Optimized Stock Levels' },
+    { url: '../../../../assets/snde7.png', title: 'Cost-Effective Transportation' },
+  ];
+  
   tiles2 = [
-    { url: '../../../../assets/snde8.png', title: 'bottom up approach' },
-    { url: '../../../../assets/snde9.png', title: 'accurate sales forecast based on last year sales' },
-    { url: '../../../../assets/snde10.png', title: 'well managed inventory levels' },
-    { url: '../../../../assets/snde11.png', title: 'system suggested forecast' },
-    { url: '../../../../assets/snde12.png', title: 'reduced cost' },
-    { url: '../../../../assets/snde13.png', title: 'efficient growth plan' },
-    { url: '../../../../assets/snde14.png', title: 'no tiresome data collection' },
-  ]
+    { url: '../../../../assets/snde8.png', title: 'Bottom-Up Approach' },
+    { url: '../../../../assets/snde9.png', title: 'Accurate Sales Forecast Based on Last Year Sales' },
+    { url: '../../../../assets/snde10.png', title: 'Well-Managed Inventory Levels' },
+    { url: '../../../../assets/snde11.png', title: 'System Suggested Forecast' },
+    { url: '../../../../assets/snde12.png', title: 'Reduced Cost' },
+    { url: '../../../../assets/snde13.png', title: 'Efficient Growth Plan' },
+    { url: '../../../../assets/snde14.png', title: 'No Tiresome Data Collection' },
+  ];
+  
   tiles3 = [
-    { url: '../../../../assets/snde15.png', title: 'system based route planning' },
-    { url: '../../../../assets/snde16.png', title: 'planogram' },
-    { url: '../../../../assets/snde17.png', title: 'promotions' },
-    { url: '../../../../assets/snde18.png', title: 'branding surveys' },
-    { url: '../../../../assets/snde19.png', title: 'competitors information for analysis and improvements' },
-    { url: '../../../../assets/snde20.png', title: 'SKU gap' },
-    { url: '../../../../assets/snde21.png', title: 'real time reporting' },
-    { url: '../../../../assets/snde22.png', title: 'mobile asset inventory' },
-    { url: '../../../../assets/snde23.png', title: 'tertiary sales input by merchandiser' },
-    { url: '../../../../assets/snde24.png', title: 'demand generation by merchandiser' },
-    { url: '../../../../assets/snde25.png', title: 'better product availability' },
-    { url: '../../../../assets/snde26.png', title: 'better product placement' },
-    { url: '../../../../assets/snde27.png', title: 'better sales' },
-  ]
+    { url: '../../../../assets/snde15.png', title: 'System-Based Route Planning' },
+    { url: '../../../../assets/snde16.png', title: 'Planogram' },
+    { url: '../../../../assets/snde17.png', title: 'Promotions' },
+    { url: '../../../../assets/snde18.png', title: 'Branding Surveys' },
+    { url: '../../../../assets/snde19.png', title: 'Competitors Information for Analysis and Improvements' },
+    { url: '../../../../assets/snde20.png', title: 'SKU Gap' },
+    { url: '../../../../assets/snde21.png', title: 'Real-Time Reporting' },
+    { url: '../../../../assets/snde22.png', title: 'Mobile Asset Inventory' },
+    { url: '../../../../assets/snde23.png', title: 'Tertiary Sales Input by Merchandiser' },
+    { url: '../../../../assets/snde24.png', title: 'Demand Generation by Merchandiser' },
+    { url: '../../../../assets/snde25.png', title: 'Better Product Availability' },
+    { url: '../../../../assets/snde26.png', title: 'Better Product Placement' },
+    { url: '../../../../assets/snde27.png', title: 'Better Sales' },
+  ];
+  
   tiles4 = [
-    { url: '../../../../assets/snde28.png', title: 'KPIs setup' },
-    { url: '../../../../assets/snde29.png', title: 'real time display of incentive to date on order booking screen' },
-    { url: '../../../../assets/snde30.png', title: 'work flows for approvals' },
-    { url: '../../../../assets/snde31.png', title: 'direct integration of incentive data with finance software' },
-    { url: '../../../../assets/snde47.png', title: 'timely disbursement of incentives' },
-    { url: '../../../../assets/snde33.png', title: 'increased trust of order booker, (eventually long term retainer-ship)' },
-  ]
+    { url: '../../../../assets/snde28.png', title: 'KPIs Setup' },
+    { url: '../../../../assets/snde29.png', title: 'Real-Time Display of Incentive to Date on Order Booking Screen' },
+    { url: '../../../../assets/snde30.png', title: 'Workflows for Approvals' },
+    { url: '../../../../assets/snde31.png', title: 'Direct Integration of Incentive Data with Finance Software' },
+    { url: '../../../../assets/snde47.png', title: 'Timely Disbursement of Incentives' },
+    { url: '../../../../assets/snde33.png', title: 'Increased Trust of Order Booker (Eventually Long Term Retainership)' },
+  ];
+  
   tiles5 = [
-    { url: '../../../../assets/snde34.png', title: 'retailer direct order placement on app' },
-    { url: '../../../../assets/snde35.png', title: 'retailer tagging with distributor or direct company' },
-    { url: '../../../../assets/snde36.png', title: 'multichannel-channel delivery' },
-    { url: '../../../../assets/snde37.png', title: 'complete traceability for retailer for order tracking' },
-    { url: '../../../../assets/snde38.png', title: 'service desk' },
-    { url: '../../../../assets/snde39.png', title: 'multi-channel order placement' },
-    { url: '../../../../assets/snde40.png', title: 'customer feedback' },
-    { url: '../../../../assets/snde41.png', title: 'less dependency of retailer on order booker for ordering' },
-    { url: '../../../../assets/snde42.png', title: 'eliminates disconnections between retailer & company' },
-  ]
+    { url: '../../../../assets/snde34.png', title: 'Retailer Direct Order Placement on App' },
+    { url: '../../../../assets/snde35.png', title: 'Retailer Tagging with Distributor or Direct Company' },
+    { url: '../../../../assets/snde36.png', title: 'Multichannel Delivery' },
+    { url: '../../../../assets/snde37.png', title: 'Complete Traceability for Retailer for Order Tracking' },
+    { url: '../../../../assets/snde38.png', title: 'Service Desk' },
+    { url: '../../../../assets/snde39.png', title: 'Multi-Channel Order Placement' },
+    { url: '../../../../assets/snde40.png', title: 'Customer Feedback' },
+    { url: '../../../../assets/snde41.png', title: 'Less Dependency of Retailer on Order Booker for Ordering' },
+    { url: '../../../../assets/snde42.png', title: 'Eliminates Disconnections Between Retailer & Company' },
+  ];
+  
   tiles6 = [
-    { url: '../../../../assets/snde43.png', title: 'distributor claims logging on portals' },
-    { url: '../../../../assets/snde44.png', title: 'tracking of claims movement/approval' },
-    { url: '../../../../assets/snde45.png', title: 'approval authority work flows' },
-    { url: '../../../../assets/snde46.png', title: 'direct integration of claims management with finance module' },
-    { url: '../../../../assets/snde47.png', title: 'timely disbursements of claims settlement' },
-    { url: '../../../../assets/snde48.png', title: 'transparency of claims tracking/treatment' },
-    { url: '../../../../assets/snde49.png', title: 'increased trust level of distributor' },
-  ]
+    { url: '../../../../assets/snde43.png', title: 'Distributor Claims Logging on Portals' },
+    { url: '../../../../assets/snde44.png', title: 'Tracking of Claims Movement/Approval' },
+    { url: '../../../../assets/snde45.png', title: 'Approval Authority Workflows' },
+    { url: '../../../../assets/snde46.png', title: 'Direct Integration of Claims Management with Finance Module' },
+    { url: '../../../../assets/snde47.png', title: 'Timely Disbursements of Claims Settlement' },
+    { url: '../../../../assets/snde48.png', title: 'Transparency of Claims Tracking/Treatment' },
+    { url: '../../../../assets/snde49.png', title: 'Increased Trust Level of Distributor' },
+  ];
+  
   tiles7 = [
-    { url: '../../../../assets/snde50.png', title: 'travel itinerary management on app' },
-    { url: '../../../../assets/snde51.png', title: 'itinerary approvals' },
-    { url: '../../../../assets/snde52.png', title: 'advance amount application on app' },
-    { url: '../../../../assets/snde53.png', title: 'GPS tracking' },
-    { url: '../../../../assets/snde54.png', title: 'visits dealing' },
-    { url: '../../../../assets/snde55.png', title: 'attendance management for visit days' },
-    { url: '../../../../assets/snde56.png', title: 'travel expenses management' },
-    { url: '../../../../assets/snde57.png', title: 'complete automation of out of city travelling' },
-    { url: '../../../../assets/snde58.png', title: 'instant approvals' },
-    { url: '../../../../assets/snde59.png', title: 'saves time of sales team' },
-    { url: '../../../../assets/snde60.png', title: 'instant disbursement of funds for travelling' },
-  ]
+    { url: '../../../../assets/snde50.png', title: 'Travel Itinerary Management on App' },
+    { url: '../../../../assets/snde51.png', title: 'Itinerary Approvals' },
+    { url: '../../../../assets/snde52.png', title: 'Advance Amount Application on App' },
+    { url: '../../../../assets/snde53.png', title: 'GPS Tracking' },
+    { url: '../../../../assets/snde54.png', title: 'Visit Detailing' },
+    { url: '../../../../assets/snde55.png', title: 'Attendance Management for Visit Days' },
+    { url: '../../../../assets/snde56.png', title: 'Travel Expenses Management' },
+    { url: '../../../../assets/snde57.png', title: 'Complete Automation of Out-of-City Traveling' },
+    { url: '../../../../assets/snde58.png', title: 'Instant Approvals' },
+    { url: '../../../../assets/snde59.png', title: 'Saves Time of Sales Team' },
+    { url: '../../../../assets/snde60.png', title: 'Instant Disbursement of Funds for Traveling' },
+  ];
+  
 }
